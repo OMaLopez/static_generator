bd379787bfa76e3ea04599feed6303c730619c55
OMaLopez <oscarmacholopez@gmail.com>
2025-07-08 23:08:17 +0200

    public to docs and basepath in generate page

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_page
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
 def main():
     print("Deleting public directory...")
@@ -24,12 +25,13 @@ def main():
         for name in files:
             if name.endswith(".md"):
                 rel_path = os.path.relpath(root, dir_path_content)
-                output_path = os.path.join(dir_path_public, rel_path, name[:-3] + ".html")
+                output_path = os.path.join(dir_path_public, rel_path,name[:-3] + ".html")
                 os.makedirs(os.path.dirname(output_path), exist_ok=True)
                 generate_page(
                     os.path.join(root, name),
                     template_path,
-                    output_path
+                    output_path,
+                    basepath
                 )
 
 
